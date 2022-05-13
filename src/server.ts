361e620c0e8b6df3f63e25c01940b8bd78805ab3
Edgar Justavino <edgarja_26@hotmail.com>
2022-05-13 17:17:50 -0300

    chore: code formatter config

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({message: 'Hello World!'})
+  return response.json({ message: 'Hello World!' });
 });
 
 const portConnection = 4000;
 
-app.listen(portConnection, ()=> {
-    console.log(`Server started on port ${portConnection}!`)
+app.listen(portConnection, () => {
+  console.log(`Server started on port ${portConnection}!`);
 });
