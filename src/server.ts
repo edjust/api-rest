3fe6df1520a0a5e5e802035570d5ae0719366c6c
Edgar Justavino <edgarja_26@hotmail.com>
2022-05-13 17:05:23 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({message: 'Hello World!'})
+});
+
+const portConnection = 4000;
+
+app.listen(portConnection, ()=> {
+    console.log(`Server started on port ${portConnection}!`)
+});
