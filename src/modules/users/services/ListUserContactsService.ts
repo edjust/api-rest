ac81b6c80a26601ceb9f008655b0330d5e064b66
Edgar Justavino <edgarja_26@hotmail.com>
2022-05-15 22:41:23 -0300

    refactor(users): list contacts service payload

--- src/modules/users/services/ListUserContactsService.ts
+++ src/modules/users/services/ListUserContactsService.ts
@@ -1,11 +1,27 @@
 import axios from 'axios';
 
+export interface Contacts {
+  contactId: string;
+  name: string;
+  phoneNumber: string;
+  email: string;
+}
+
 export class ListUserContactsService {
-  public async execute(userId: number): Promise<[]> {
+  public async execute(userId: number): Promise<Contacts[]> {
     const apiURL = `https://${process.env.HOST_PARAM}.mockapi.io/api/v1/users/${userId}/contacts`;
 
     const { data } = await axios.get(apiURL);
-    const userContacts = data;
+
+    let contactIdCont = 1;
+    let userContacts: Contacts[] = [];
+
+    data.map((contact: Contacts) => {
+      contact.contactId = String(contactIdCont);
+      contactIdCont++;
+      let { contactId, name, phoneNumber, email } = contact;
+      userContacts.push({ contactId, name, phoneNumber, email });
+    });
 
     return userContacts;
   }
