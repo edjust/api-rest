d4c4a500bdd5348990148ed3eaea7b652364cd27
Edgar Justavino <edgarja_26@hotmail.com>
2022-05-15 22:40:39 -0300

    refactor(users): list addresses service payload

--- src/modules/users/services/ListUserAddressesService.ts
+++ src/modules/users/services/ListUserAddressesService.ts
@@ -1,11 +1,49 @@
 import axios from 'axios';
 
+export interface Request {
+  addressId: string;
+  street: string;
+  number: number;
+  country: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
+
+export interface Addresses {
+  addressId: string;
+  address: string;
+  country: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
+
 export class ListUserAddressesService {
-  public async execute(userId: number): Promise<[]> {
+  public async execute(userId: number): Promise<Addresses[]> {
     const apiURL = `https://${process.env.HOST_PARAM}.mockapi.io/api/v1/users/${userId}/address`;
 
     const { data } = await axios.get(apiURL, { timeout: 5000 });
-    const userAddresses = data;
+
+    let addressIdCont = 1;
+    let userAddresses: Addresses[] = [];
+
+    data.map((addresses: Request) => {
+      addresses.addressId = String(addressIdCont);
+      addressIdCont++;
+
+      let { addressId, street, number, country, city, state, zipcode } =
+        addresses;
+
+      userAddresses.push({
+        addressId,
+        address: street + ', ' + number,
+        country,
+        city,
+        state,
+        zipcode,
+      });
+    });
 
     return userAddresses;
   }
